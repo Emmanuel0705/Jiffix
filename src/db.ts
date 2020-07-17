5e6beab40057412e66d1f0aa1dceca51cd6b41cf
Oluwatobi <emmanuelloluwatobi@gmail.com>
2020-07-17 06:19:45 +0100

    making changes

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,13 @@
 import knex from 'knex';
 
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 const db = knex({
     client: 'mysql',
     connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
     },
 });
 
